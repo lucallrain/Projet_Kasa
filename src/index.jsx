342a261f3745c819f8f9b8855231a4de672cd029
Luca <luca.lelaurain@gmail.com>
2024-09-04 10:48:06 +0200

    refacto router

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,31 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {createBrowserRouter,RouterProvider,} from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Error from './components/error/error';
 import Accommodation from './components/accommodation/accommodation';
+import ErrorPage from './components/error/error';
+import AppLayout from './components/layout/layout'; 
 import './index.scss';
-import HomeLogo from './assets/home-logo.png';
-import AboutLogo from './assets/about-logo.png';
 
-const navLinks = [
-  { path: '/', label: 'Acceuil', logo: HomeLogo },
-  { path: '/about', label: 'À propos', logo: AboutLogo },
-];
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />, 
+    errorElement: <ErrorPage />, 
+    children: [
+      { index: true, element: <Home /> }, 
+      { path: 'about', element: <About /> },
+      { path: 'accommodation/:id', element: <Accommodation /> },
+      { path: 'error', element: <ErrorPage /> },
+      { path: '*', element: <ErrorPage /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <div className='content'>
-      <Header navLinks={navLinks} defaultLogo={HomeLogo} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Error />} />
-          <Route path="/accommodation/:id" element={<Accommodation />} />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
