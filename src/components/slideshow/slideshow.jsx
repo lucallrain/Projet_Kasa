73fe171864868982f73ff7d0fecb78afd6364b91
Luca <luca.lelaurain@gmail.com>
2024-09-11 14:34:22 +0200

    correction slideshow

--- src/components/slideshow/slideshow.jsx
+++ src/components/slideshow/slideshow.jsx
@@ -3,16 +3,19 @@ import './slideshow.scss'
 
 export default function Slideshow({ pictures }) {
   const [slide, setSlide] = useState(0)
+
   const goToPrevious = () => {
     const isFirstSlide = slide === 0
     const newIndex = isFirstSlide ? pictures.length - 1 : slide - 1
     setSlide(newIndex)
   }
+
   const goToNext = () => {
     const isLastSlide = slide === pictures.length - 1
     const newIndex = isLastSlide ? 0 : slide + 1
     setSlide(newIndex)
   }
+
   return (
     <div className="slideshow__container">
       <img
@@ -20,9 +23,13 @@ export default function Slideshow({ pictures }) {
         alt="carrousel du logement"
         className="slideshow__container__img"
       />
-      <i className="fa-solid fa-chevron-left" onClick={goToPrevious}></i>
-      <i className="fa-solid fa-chevron-right" onClick={goToNext}></i>
-      <p className='slideshow__container__txt'>{slide + 1}/{pictures.length}</p>
+      {pictures.length > 1 && (
+        <>
+          <i className="fa-solid fa-chevron-left" onClick={goToPrevious}></i>
+          <i className="fa-solid fa-chevron-right" onClick={goToNext}></i>
+          <p className="slideshow__container__txt">{slide + 1}/{pictures.length}</p>
+        </>
+      )}
     </div>
   )
 }
